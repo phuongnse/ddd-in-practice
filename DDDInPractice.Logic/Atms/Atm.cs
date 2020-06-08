4ea61ce1d83939e37c9729798d7d64ddc3ccd805
Phuong Nguyen <phuongnse@gmail.com>
2020-06-08 09:30:16 +0700

    Add Atm specs

--- DDDInPractice.Logic/Atms/Atm.cs
+++ DDDInPractice.Logic/Atms/Atm.cs
@@ -1,8 +1,42 @@
 ﻿using DDDInPractice.Logic.Common;
+using DDDInPractice.Logic.SharedKernel;
+using static DDDInPractice.Logic.SharedKernel.Money;
 
 namespace DDDInPractice.Logic.Atms
 {
     public class Atm : AggregateRoot
     {
+        private const decimal CommissionRate = 0.01m;
+
+        public Atm()
+        {
+            MoneyInside = None;
+            MoneyCharged = 0;
+        }
+
+        public virtual Money MoneyInside { get; protected set; }
+        public virtual decimal MoneyCharged { get; protected set; }
+
+        public virtual void LoadMoney(Money money)
+        {
+            MoneyInside += money;
+        }
+
+        public virtual void TakeMoney(decimal amount)
+        {
+            MoneyInside -= MoneyInside.Allocate(amount);
+            MoneyCharged += CalculateAmountWithCommission(amount);
+        }
+
+        private static decimal CalculateAmountWithCommission(decimal amount)
+        {
+            var commission = amount * CommissionRate;
+            var lessThanOneCent = commission % 0.01m;
+
+            if (lessThanOneCent > 0)
+                commission = commission - lessThanOneCent + 0.01m;
+
+            return amount + commission;
+        }
     }
 }
---
+++ DDDInPractice.Test/AtmSpecs.cs
@@ -0,0 +1,44 @@
+﻿using DDDInPractice.Logic.Atms;
+using FluentAssertions;
+using Xunit;
+using static DDDInPractice.Logic.SharedKernel.Money;
+
+namespace DDDInPractice.Test
+{
+    public class AtmSpecs
+    {
+        [Fact]
+        public void TakeMoney_ShouldApplyCommissionWithAtLeastOneCent()
+        {
+            var atm = new Atm();
+            atm.LoadMoney(OneCent);
+
+            atm.TakeMoney(0.01m);
+
+            atm.MoneyCharged.Should().Be(0.02m);
+        }
+
+        [Fact]
+        public void TakeMoney_ShouldExchangesMoneyWithCommission()
+        {
+            var atm = new Atm();
+            atm.LoadMoney(OneDollar);
+
+            atm.TakeMoney(1);
+
+            atm.MoneyInside.Should().Be(None);
+            atm.MoneyCharged.Should().Be(1.01m);
+        }
+
+        [Fact]
+        public void TakeMoney_ShouldRoundedUpCommissionToTheNextCent()
+        {
+            var atm = new Atm();
+            atm.LoadMoney(OneDollar + TenCent);
+
+            atm.TakeMoney(1.1m);
+
+            atm.MoneyCharged.Should().Be(1.12m);
+        }
+    }
+}
